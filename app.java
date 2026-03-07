8834f5917ab2c13295e389ccb862c3848d97a725
gandlavs8 <gandlavs8@gmail.com>
2026-03-07 21:57:26 +0530

    Add Main class with a Hello World program

---
+++ app.java
@@ -0,0 +1,8 @@
+// Main.java - The class name must match the filename
+public class Main {
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+        // System.out.println prints the specified message to the console
+        System.out.println("Hello, World!");
+    }
+}
